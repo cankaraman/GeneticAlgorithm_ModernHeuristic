4c287cfcbd586f43a8fd9b88eede166996409525
xcankar <ycankaraman@gmail.com>
2018-11-20 15:12:04 +0300

    works except for constraint handling

--- genetic_tsp.py
+++ genetic_tsp.py
@@ -1,63 +1,66 @@
 import random
+import pdb
+
+TOURNEMET_SIZE = 2
+
 
 class GeneticAlgorithm:
-    TOURNEMET_SIZE = 2
 
     def __init__(self, generation_count, population_size, crossover_rate,
-                 mutation_rate, genom_size):
+                 mutation_rate):
         self.generation_count = generation_count
         self.population_size = population_size
         self.crossover_rate = crossover_rate
         self.mutation_rate = mutation_rate
-        self.genom_size = genom_size
+        self.genom_size = 0
         self.capacity = 0
         self.weights = []
         self.values = []
         self.load_data()
 
-    def load_data(self)
-        file_header = ""
-        problem = ""
+    def load_data(self):
+        problem = "p06"
 
-        #read values
-        file_type = ""
-        file_name = file_header + problem + file_type
+        # read values
+        file_type = "_p.txt"
+        file_name = problem + file_type
         file = open(file_name, "r")
         for line in file:
             self.values.append(int(line))
         file.close()
 
-        #read capacity
-        file_type = ""
-        file_name = file_header + problem + file_type
+        # pdb.set_trace()
+        self.genom_size = len(self.values)
+
+        # read capacity
+        file_type = "_c.txt"
+        file_name = problem + file_type
         file = open(file_name, "r")
         capacity = int(file.readline())
         file.close()
 
-        #read weights
-        file_type = ""
-        file_name = file_header + problem + file_type
+        # read weights
+        file_type = "_w.txt"
+        file_name = problem + file_type
         file = open(file_name, "r")
         for line in file:
             self.weights.append(int(line))
         file.close()
 
-
     def flip_bit(self, genom, index):
-        newBitArray = genom.copy()
-        newBitArray[index] = ~bit_array[index]+2
-        return newBitArray
+        new_genom = genom.copy()
+        new_genom[index] = ~genom[index]+2
+        return new_genom
 
     def evaluate(self, genom):
         knapsack_value = 0
         knapsack_weight = 0
         for i in range(self.genom_size):
             if genom[i] == 1:
-                knapsack_value = self.values[i]
-                knapsack_weight = self.weights[i]
+                knapsack_value += self.values[i]
+                knapsack_weight += self.weights[i]
 
         return {'value': knapsack_value, 'weight': knapsack_weight}
-
 
     def initialize_population(self):
         return [self.initialize_genom() for _ in range(self.population_size)]
@@ -67,10 +70,11 @@ class GeneticAlgorithm:
 
     def create_childs(self, parents):
         # crossover parents using random index
-        index = random.randint(0, genom_size)
+        index = random.randint(0, self.genom_size)
 
         i = 0
-        if random.random() < crossover_rate:
+        if random.random() < self.crossover_rate:
+            # pdb.set_trace()
             child1 = parents[i][:index]
             child1.extend(parents[i+1][index:])
 
@@ -81,19 +85,29 @@ class GeneticAlgorithm:
             child2 = parents[1]
 
         # mutation
-        if random.random() < mutation_rate:
-            mutation_index = random.randint(bit_count*2)
-            if mutation_index >= bit_count:
-                mutation_index -= bit_count
+        if random.random() < self.mutation_rate:
+            mutation_index = random.randint(0, self.genom_size*2 - 1)
+            if mutation_index >= self.genom_size:
+                mutation_index -= self.genom_size
                 child1 = self.flip_bit(child1, mutation_index)
             else:
                 child2 = self.flip_bit(child2, mutation_index)
 
         return child1, child2
+    def get_worst_genom(self, genoms):
+        worst_fitness = 0
+        worst_genom = []
+
+        for genom in genoms:
+            current_fitness = self.evaluate(genom)['value']
+            if current_fitness > worst_fitness:
+                worst_genom = genom
+
+        return worst_genom
 
     def get_best_genom(self, genoms):
         best_fitness = 0
-        best_genom = genoms[0]
+        best_genom = []
 
         for genom in genoms:
             current_fitness = self.evaluate(genom)['value']
@@ -102,30 +116,36 @@ class GeneticAlgorithm:
 
         return best_genom
 
-    def tournement_selection(genoms):
+    def tournement_selection(self, genoms):
         contenders = []
         t_size = TOURNEMET_SIZE
         for i in range(t_size):
-            random_index = random.randint(0, population_size)
+            random_index = random.randint(0, self.population_size - 1)
             contenders.append(genoms[random_index])
 
-        return get_best_genom(contenders)
+        return self.get_best_genom(contenders)
 
     def run(self):
-        # not yet rearranged
         population = self.initialize_population()
         very_best_genom_fitness = 0
         very_best_genom = population[0]
 
         for i in range(self.generation_count):
             childs = []
-            for j in range(self.population_size / 2):
-                parents = select_parents(population, TOURNEMET_SIZE)
-                childs.extend(create_childs(
-                    parents, crossover_rate, mutation_rate, bit_count))
+            for j in range(int(self.population_size / 2)):
+                parents = [self.tournement_selection(population) for _ in
+                           range(2)]
+                childs.extend(self.create_childs(parents))
 
-            current_genom = get_best_genom(childs, bit_count)
-            current_genom_fitness = ht.evaluate(current_genom)
+            #bootleg elitism. delete the worst add the best from previous
+            #generaion
+            best_previous = self.get_best_genom(population)
+            population = childs.copy()
+            population.remove(self.get_worst_genom(population))
+            population.append(best_previous)
+
+            current_genom = self.get_best_genom(childs)
+            current_genom_fitness = self.evaluate(current_genom)['value']
             if very_best_genom_fitness < current_genom_fitness:
                 very_best_genom_fitness = current_genom_fitness
                 very_best_genom = current_genom
